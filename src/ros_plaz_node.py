fefec31c39cdc723203305d92854d8554323beac
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2021-10-19 14:49:55 +0300

    Update ros_plaz_node.py

--- src/ros_plaz_node.py
+++ src/ros_plaz_node.py
@@ -302,10 +302,10 @@ class ROSPlazNode(): # класс ноды ros_plaz_node
                         local_point.z = self.messenger.hub['USNav_module']['z'].read()[0] * 0.001
                         self.local_position_publisher.publish(local_point)
 
-                        self.local_yaw_publisher.publish(self.messenger.hub['USNav_module']['yaw'].read()[0])
+                        self.local_yaw_publisher.publish(self.messenger.hub['USNav_module']['yaw'].read()[0] * 0.001)
 
                         self.local_status.publish(self.messenger.hub['USNav_module']['status'].read()[0])
-                    except Exception as e:
+                    except:
                         rospy.logerr("LPS not found")
                 elif self.navSystem == 2:
                     try:
